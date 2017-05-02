d956f3564622e6567fb2839e66accd08d4f2840e
Dana Franco <turquoise.ilg@hotmail.com>
2017-05-02 17:26:22 -0500

    fixing footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,40 @@
+document.getElementById("input-number").addEventListener("click", function(){
+    //document.getElementById("demo").innerHTML = "Hello World";
+    var input1 = document.createElement("input");
+    input1.setAttribute("type","text");
+    input1.setAttribute("placeholder","Name");
+    //document.body.appendChild(input1);
+    document.getElementById("box").appendChild(input1);
+
+    var br1 = document.createElement("br");
+    document.getElementById("box").appendChild(br1);
+
+    var br1 = document.createElement("br");
+    document.getElementById("box").appendChild(br1);
+
+    var input2 = document.createElement("input");
+    input2.setAttribute("type","email");
+    input2.setAttribute("placeholder","Email");
+    document.getElementById("box").appendChild(input2);
+
+    var br1 = document.createElement("br");
+    document.getElementById("box").appendChild(br1);
+
+    var br1 = document.createElement("br");
+    document.getElementById("box").appendChild(br1);
+
+    var input3 = document.createElement("input");
+    input3.setAttribute("type","text");
+    input3.setAttribute("placeholder","City");
+    document.getElementById("box").appendChild(input3);
+
+    var br1 = document.createElement("br");
+    document.getElementById("box").appendChild(br1);
+
+    var br1 = document.createElement("br");
+    document.getElementById("box").appendChild(br1);
+});
+
 var number = document.getElementById("input-number");
 
 if(number.value.length < 10){
